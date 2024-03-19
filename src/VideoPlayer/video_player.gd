e41b4668ca8a75e73d61459a320674f4b0ef194f
Evan McGrath <34551781+Tougon@users.noreply.github.com>
2024-03-19 14:17:03 -0700

    3-19-24 - Background adjustments
    More experimentation with backgrounds

--- src/VideoPlayer/video_player.gd
+++ src/VideoPlayer/video_player.gd
@@ -1,9 +1,15 @@
 extends VideoStreamPlayer
 class_name LoopingVideoPlayer
 
+@export var delay_start : bool = false;
+@export var delay_time : float = 0.1;
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	finished.connect(on_finished);
+	
+	if delay_start : await get_tree().create_timer(delay_time).timeout
+	play();
 
 
 func on_finished():
