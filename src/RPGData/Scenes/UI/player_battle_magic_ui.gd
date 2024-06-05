07a63cfdb0fb9bf63f0b9cd2f344d9789970930d
Evan McGrath <34551781+Tougon@users.noreply.github.com>
2024-06-04 18:38:11 -0700

    6-4-24 - Spell UI navigation
    Added wraparound for spell UI navigation

--- src/RPGData/Scenes/UI/player_battle_magic_ui.gd
+++ src/RPGData/Scenes/UI/player_battle_magic_ui.gd
@@ -2,6 +2,7 @@ extends MenuPanel
 
 var current_entity : EntityController;
 @export var button_count : int = 30;
+@export var buttons_per_row : int = 3;
 @export var move_button : PackedScene;
 @export var button_spacer : PackedScene;
 @export var button_root : Node;
@@ -27,9 +28,11 @@ func _ready():
 func _set_active_entity(entity : EntityController):
 	current_entity = entity;
 
+
 func _initialize_magic_menu(entity : EntityController):
 	var move_list = entity.move_list;
 	var default_selection = true;
+	var last_row_index = _get_bottom_row_start_index(move_list.size());
 	
 	for i in move_list.size():
 		var move = move_list[i];
@@ -42,12 +45,39 @@ func _initialize_magic_menu(entity : EntityController):
 		(all_selections[i] as MagicButtonUI).init_button(move);
 		all_selections[i].visible = true;
 		all_selections[i].disabled = entity.current_mp < move.spell_cost;
+		
+		# If we're in the first row, wrap to the bottom
+		if i < buttons_per_row :
+			var index = last_row_index + (i % buttons_per_row);
+			while index >= move_list.size() : index -= 1;
+			all_selections[i].focus_neighbor_top = all_selections[index].get_path();
+		
+		# If we're in the bottom row, wrap to the top
+		if i >= last_row_index:
+			var index = i % buttons_per_row;
+			all_selections[i].focus_neighbor_bottom = all_selections[index].get_path();
+		
+		# At the start and end of a row, wrap around.
+		if i % buttons_per_row == 0:
+			var index = i + buttons_per_row - 1;
+			while index >= move_list.size(): index -= 1;
+			all_selections[i].focus_neighbor_left = all_selections[index].get_path();
+		elif i % buttons_per_row == buttons_per_row - 1 :
+			var index = i;
+			while index % buttons_per_row != 0: index -= 1;
+			all_selections[i].focus_neighbor_right = all_selections[index].get_path();
 	
 	if default_selection && move_list.size() > 0:
 		initial_selection = all_selections[0];
 	
 	for i in range(move_list.size(), all_selections.size()):
 		all_selections[i].visible = false;
+
+
+func _get_bottom_row_start_index(size : int) -> int:
+	if size < buttons_per_row : return size - 1;
+	elif size % buttons_per_row == 0 : return size - buttons_per_row;
+	return size - (size % buttons_per_row);
 
 
 func on_menu_cancel():
