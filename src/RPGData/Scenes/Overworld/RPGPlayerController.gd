68c87bc3f027a6ab83c1357edf184f93af50ff37
Evan McGrath <34551781+Tougon@users.noreply.github.com>
2024-08-27 18:39:37 -0700

    8-27-24 - Player Controller (Early)
    Started work on overworld crud. Very early because I wasted two hours overthinking Raw Input...

---
+++ src/RPGData/Scenes/Overworld/RPGPlayerController.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+class_name RPGPlayerController
+
+@export var speed : float = 250.0;
+var movement_bounds : float = 0.5;
+#TODO: Modify how the sprites work entirely.
+
+func _ready():
+	pass;
+
+func _process(_delta):
+	var h = Input.get_axis("move_l","move_r");
+	var v = Input.get_axis("move_u", "move_d");
+	
+	var angle = rad_to_deg(Vector2(h, v).angle());
+	
+	h = h/abs(h) if abs(h) >= movement_bounds else 0;
+	v = v/abs(v) if abs(v) >= movement_bounds else 0;
+	
+	var direction = Vector2(h, v).normalized();
+	move(direction);
+	
+	# Debug
+	$Sprite2D2.position = direction * 125
+	if direction.length() > 0.5 : 
+		$Sprite2D2.rotation_degrees = rad_to_deg(direction.angle()) - 90;
+	else : $Sprite2D2.rotation_degrees = 0;
+	# End Debug
+
+
+func move(direction : Vector2):
+	velocity = direction * speed;
+	move_and_slide();
