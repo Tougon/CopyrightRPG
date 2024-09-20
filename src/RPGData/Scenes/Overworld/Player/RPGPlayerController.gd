624ef4dfb1fefe684b15d7e4ad186f6c188285d3
Evan McGrath <34551781+Tougon@users.noreply.github.com>
2024-09-20 14:59:31 -0700

    9-20-24 - Interaction
    Added templates for "A button" interaction

--- src/RPGData/Scenes/Overworld/Player/RPGPlayerController.gd
+++ src/RPGData/Scenes/Overworld/Player/RPGPlayerController.gd
@@ -5,9 +5,13 @@ class_name RPGPlayerController
 @export_range(1,10) var run_multiplier : float = 1.5;
 @export_range(0.1, 1) var skid_duration : float = 0.25;
 
-var movement_bounds : float = 0.5;
-var prev_direction : Vector2;
-var can_move : bool = true;
+const COLLISION_DETECTION_RANGE : float  = 80.0;
+
+var direction_facing : Vector2 = Vector2(0, 1);
+
+var _movement_bounds : float = 0.5;
+var _prev_direction : Vector2;
+var _can_move : bool = true;
 
 #TODO: Modify how the sprites work entirely.
 
@@ -18,8 +22,9 @@ func _ready():
 	UIManager.on_menu_opened.connect(_on_menu_opened);
 	UIManager.on_all_menus_closed.connect(_on_all_menus_closed);
 
+
 func _process(_delta):
-	if !can_move : return;
+	if !_can_move : return;
 	
 	# Handle pause input
 	if Input.is_action_just_pressed("pause"):
@@ -31,14 +36,16 @@ func _process(_delta):
 	move(direction);
 	
 	# Debug
-	$Sprite2D2.position = direction * 125
-	if direction.length() > 0.5 : 
-		$Sprite2D2.rotation_degrees = rad_to_deg(direction.angle()) - 90;
+	# TODO: Fix so that diagonals work properly. 1,1 is correct here ONLY
+	$Sprite2D2.position = (direction_facing) * COLLISION_DETECTION_RANGE;
+	if direction_facing.length() > 0.5 : 
+		$Sprite2D2.rotation_degrees = rad_to_deg(direction_facing.angle()) - 90;
 	else : $Sprite2D2.rotation_degrees = 0;
 	# End Debug
 	
-	if Input.is_action_just_pressed("interact") && can_move:
-		print("Interact ping")
+	if Input.is_action_just_pressed("interact") && _can_move:
+		if $"Direction Root/Sight".closest_interactable != null :
+			print($"Direction Root/Sight".closest_interactable.name);
 
 
 func _get_movement_vector() -> Vector2:
@@ -47,8 +54,8 @@ func _get_movement_vector() -> Vector2:
 	
 	var angle = rad_to_deg(Vector2(h, v).angle());
 	
-	h = h/abs(h) if abs(h) >= movement_bounds else 0;
-	v = v/abs(v) if abs(v) >= movement_bounds else 0;
+	h = h/abs(h) if abs(h) >= _movement_bounds else 0;
+	v = v/abs(v) if abs(v) >= _movement_bounds else 0;
 	
 	var direction = Vector2(h, v).normalized();
 	return direction;
@@ -66,20 +73,23 @@ func move(direction : Vector2):
 	if delta_pos.length() > 0 : 
 		EventManager.on_overworld_player_moved.emit(direction, velocity);
 	
-	if Input.is_action_just_released("run") || (Input.is_action_pressed("run") && prev_direction != direction && prev_direction.length_squared() > 0):
-		var angle_dif = abs(rad_to_deg(prev_direction.angle_to(direction)))
+	if direction.length_squared() != 0:
+		direction_facing = direction;
+	
+	if Input.is_action_just_released("run") || (Input.is_action_pressed("run") && _prev_direction != direction && _prev_direction.length_squared() > 0):
+		var angle_dif = abs(rad_to_deg(_prev_direction.angle_to(direction)))
 		# We have to use 91 here due to bizarre rounding errors
 		if angle_dif == 0 || angle_dif > 91 : 
-			skid(prev_direction * speed * run_multiplier, prev_direction * speed);
+			skid(_prev_direction * speed * run_multiplier, _prev_direction * speed);
 	
-	if prev_direction != direction: prev_direction = direction;
+	if _prev_direction != direction: _prev_direction = direction;
 
 
 func skid(initial : Vector2, final : Vector2):
 	var direction = initial.normalized();
 	var orig_velocity = initial;
 	var time = 0;
-	can_move = false;
+	_can_move = false;
 	
 	while (time < skid_duration && skid_duration > 0):
 		await get_tree().process_frame;
@@ -88,8 +98,8 @@ func skid(initial : Vector2, final : Vector2):
 		move_and_slide();
 		time += get_process_delta_time();
 	
-	prev_direction = _get_movement_vector();
-	can_move = true;
+	_prev_direction = _get_movement_vector();
+	_can_move = true;
 
 
 func _on_overworld_battle_queued(encounter : Encounter):
---
+++ src/RPGData/Scenes/Overworld/Player/RPGPlayerInteractArea.gd
@@ -0,0 +1,56 @@
+extends Area2D
+
+var interactions : Array[Interactable];
+var closest_interactable : Interactable;
+
+func _ready():
+	area_entered.connect(_on_area_entered)
+	body_entered.connect(_on_body_entered)
+	area_exited.connect(_on_area_exited)
+	body_exited.connect(_on_body_exited)
+
+
+func _on_area_entered(area : Area2D):
+	_add_node_to_list(area.get_parent())
+
+
+func _on_body_entered(body : Node2D):
+	_add_node_to_list(body.get_parent())
+
+
+func _on_area_exited(area : Area2D):
+	_remove_node_from_list(area.get_parent())
+
+
+func _on_body_exited(body : Node2D):
+	_remove_node_from_list(body.get_parent())
+
+
+func _add_node_to_list(node : Node):
+	if node != null && node is Interactable:
+		interactions.append(node as Interactable);
+		_calculate_closest_interactable();
+
+
+func _remove_node_from_list(node : Node):
+	if node != null && node is Interactable:
+		interactions.erase(node as Interactable);
+		_calculate_closest_interactable();
+
+
+func _calculate_closest_interactable():
+	var prev_closest = closest_interactable;
+	closest_interactable = null;
+	
+	# For some reason, Godot does not support max for float
+	var distance = 1000000000;
+	
+	for obj in interactions:
+		var dist = obj.global_position.distance_to(global_position);
+		if  dist < distance:
+			closest_interactable = obj;
+			distance = dist;
+	
+	if prev_closest != closest_interactable:
+		if prev_closest != null: prev_closest.highlight(false);
+		if closest_interactable != null: closest_interactable.highlight(true);
