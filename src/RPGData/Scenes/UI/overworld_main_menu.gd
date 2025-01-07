f241e9a91af5aeed02015fe9c8a08fcf17628eb6
Evan McGrath <34551781+Tougon@users.noreply.github.com>
2025-01-07 14:31:17 -0800

    1-7-25 - More UI Stuff
    Mostly pushing to create a restore point

--- src/RPGData/Scenes/UI/overworld_main_menu.gd
+++ src/RPGData/Scenes/UI/overworld_main_menu.gd
@@ -1,5 +1,8 @@
 extends MenuPanel
 
+@export var header_icons : Array[TweenPlayer];
+var _current_tab_index : int = 4;
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	super._ready();
@@ -15,3 +18,38 @@ func _on_load_pressed():
 
 func _on_delete_pressed():
 	DataManager.delete_data();
+
+
+func on_menu_active():
+	super.on_menu_active();
+	
+	_play_header_tweens();
+
+
+func on_menu_inactive():
+	super.on_menu_inactive();
+	
+	for index in header_icons.size():
+		header_icons[index].play_tween_name("Focus Exited");
+
+
+func _play_header_tweens():
+	for index in header_icons.size():
+		if _current_tab_index == index :
+			header_icons[index].play_tween_name("Focus Entered");
+		else :
+			header_icons[index].play_tween_name("Focus Exited");
+
+
+func on_ui_aux_1():
+	_current_tab_index -= 1;
+	if _current_tab_index < 0 : _current_tab_index = header_icons.size() - 1;
+	
+	_play_header_tweens();
+
+
+func on_ui_aux_2():
+	_current_tab_index += 1;
+	if _current_tab_index >= header_icons.size() : _current_tab_index = 0;
+	
+	_play_header_tweens();
