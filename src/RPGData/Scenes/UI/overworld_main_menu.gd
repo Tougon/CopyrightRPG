a0fbcf139ba74acc9928edf0f6a4b9b20fa97298
Evan McGrath <34551781+Tougon@users.noreply.github.com>
2025-01-07 15:28:33 -0800

    1-7-25 - Animation Polish
    Fixed up the open/close animations for main menu

--- src/RPGData/Scenes/UI/overworld_main_menu.gd
+++ src/RPGData/Scenes/UI/overworld_main_menu.gd
@@ -6,6 +6,7 @@ var _current_tab_index : int = 4;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	super._ready();
+	$"Header/Options Headers/GridContainer".modulate = Color.TRANSPARENT;
 
 
 func _on_save_pressed():
@@ -20,6 +21,21 @@ func _on_delete_pressed():
 	DataManager.delete_data();
 
 
+func set_active(state : bool):
+	super.set_active(state);
+	
+	if !state :
+		for index in header_icons.size():
+			header_icons[index].play_tween_name("Focus Exited");
+	else :
+		await get_tree().create_timer(0.25).timeout;
+		_play_header_tweens();
+
+
+func set_active_forced(state : bool):
+	super.set_active_forced(state);
+
+
 func on_menu_active():
 	super.on_menu_active();
 	
@@ -28,9 +44,6 @@ func on_menu_active():
 
 func on_menu_inactive():
 	super.on_menu_inactive();
-	
-	for index in header_icons.size():
-		header_icons[index].play_tween_name("Focus Exited");
 
 
 func _play_header_tweens():
