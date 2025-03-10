ffd34ae867bcc4574f20746958d328c445876f75
Evan McGrath <34551781+Tougon@users.noreply.github.com>
2025-03-10 09:18:03 -0700

    3-10-25 - CPU Randomization
    Targets can now be randomized for the HP check

--- src/RPGData/Scripts/Behavior/BehaviorChecks/BehaviorCheckHP.gd
+++ src/RPGData/Scripts/Behavior/BehaviorChecks/BehaviorCheckHP.gd
@@ -3,6 +3,7 @@ extends BehaviorCheck
 class_name BehaviorCheckHP
 
 @export_range(0, 1) var hp_percent : float;
+@export var randomize_target : bool;
 
 func check(user : EntityController, allies : Array[EntityController], targets : Array[EntityController], result : BehaviorCheckResult) -> bool:
 	
@@ -20,6 +21,8 @@ func check(user : EntityController, allies : Array[EntityController], targets : 
 		if check_target == CheckTarget.Allies:
 			check_ec = allies;
 		
+		var entities : Array[EntityController];
+		
 		for ec in check_ec:
 			var percent_check = ec.get_hp_percent() >= hp_percent;
 			var val = false;
@@ -31,6 +34,11 @@ func check(user : EntityController, allies : Array[EntityController], targets : 
 			
 			if val:
 				result.trigger_entity = ec;
-				return true;
+				if !randomize_target : return true;
+				else : entities.append(ec);
+		
+		if randomize_target && entities.size() > 0 :
+			result.trigger_entity = entities.pick_random();
+			return true;
 		
 	return false;
