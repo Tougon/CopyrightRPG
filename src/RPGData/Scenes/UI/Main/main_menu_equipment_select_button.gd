f7438fd7101e85b982599fd063c4ce1092428557
Evan McGrath <34551781+Tougon@users.noreply.github.com>
2025-06-26 07:59:39 -0700

    6-26-25 - Removing old UI buttons
    Title. We've redesigned the Equipment/Ability selection to use radial carousel instead of dynamic menu making these items redundant. For the future, please just change the scripting/scene for the existing buttons.

--- src/RPGData/Scenes/UI/Main/main_menu_equipment_select_button.gd
+++
@@ -1,7 +0,0 @@
-extends Button
-
-func refresh_data(data):
-	if data is Item :
-		if data == null : text = "-";
-		else : text = tr(data.item_name_key);
-	else : text = "-";
--- src/RPGData/Scenes/UI/Main/main_menu_move_select_button.gd
+++
@@ -1,7 +0,0 @@
-extends Button
-
-func refresh_data(data):
-	if data is Spell :
-		if data == null : text = "-";
-		else : text = tr(data.spell_name_key);
-	else : text = "-";
