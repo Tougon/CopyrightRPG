2e377033ca2196d3fc93784baf12c78830848295
Evan McGrath <34551781+Tougon@users.noreply.github.com>
2025-07-14 15:38:46 -0700

    7-14-25 - Divide by Zero Fix
    Fixed issue where a divide by zero was possible in the damage calculation

--- src/RPGData/Scripts/Spells/damage_spell.gd
+++ src/RPGData/Scripts/Spells/damage_spell.gd
@@ -178,7 +178,8 @@ func calculate_damage(user : EntityController, target : EntityController, cast :
 		else : crit_chance *= user.param.entity_crit_chance_modifier;
 		
 		# If target crit resist modifier is 0, target cannot be crit
-		crit_chance /= target.param.entity_crit_resist_modifier
+		if target.param.entity_crit_resist_modifier == 0 : crit_chance = 0;
+		else : crit_chance /= target.param.entity_crit_resist_modifier;
 		
 		var critical = false;
 		if critical_chance > 0 && can_critical:
