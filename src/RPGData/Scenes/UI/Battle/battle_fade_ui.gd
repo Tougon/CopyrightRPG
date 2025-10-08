0a38e0fe9cd18be163d5dad79d44a9f52f05d939
Evan McGrath <34551781+Tougon@users.noreply.github.com>
2025-10-08 12:18:34 -0700

    10-8-25 - Battle Transition Changes
    Replaced Battle In transition

--- src/RPGData/Scenes/UI/Battle/battle_fade_ui.gd
+++ src/RPGData/Scenes/UI/Battle/battle_fade_ui.gd
@@ -1,7 +1,8 @@
 extends CanvasLayer
 
-@export var fade_sequence : TweenPlayer;
-@export var camera_fade_sequence : TweenPlayer;
+@export var fade_in_player : TweenPlayer;
+@export var fade_out_player : TweenPlayer;
+@export var camera_fade_player : TweenPlayer;
 
 func _ready():
 	self.visible = true;
@@ -9,13 +10,26 @@ func _ready():
 
 
 func _fade_action(fade_in : bool):
-	if fade_sequence != null:
-		if fade_in:
-			fade_sequence.play_tween_name("Fade In");
-			camera_fade_sequence.play_tween_name("Transition Zoom In");
-		else:
-			fade_sequence.play_tween_name("Fade Out");
-		await fade_sequence.tween_ended;
+	if fade_in_player : fade_in_player.get_parent().visible = fade_in;
+	if fade_out_player : fade_out_player.get_parent().visible = !fade_in;
+	
+	if fade_in:
+		if camera_fade_player != null :
+			camera_fade_player.play_tween_name("Transition Zoom In");
+		
+		if fade_out_player != null :
+			fade_out_player.play_tween_name("Reset");
+		
+		if fade_in_player != null:
+			fade_in_player.play_tween_name("Fade In");
+			await fade_in_player.tween_ended;
+	else:
+		if fade_out_player != null :
+			fade_out_player.play_tween_name("Fade Out");
+			await fade_out_player.tween_ended;
+			
+			if fade_in_player != null :
+				fade_in_player.play_tween_name("Reset");
 	
 	EventManager.battle_fade_completed.emit(fade_in);
 
