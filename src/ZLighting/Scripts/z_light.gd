26d28c444bed1e3f747867e640c02c620f5aa1eb
Evan McGrath <34551781+Tougon@users.noreply.github.com>
2025-11-13 20:55:16 -0800

    11-13-25 - Fake Lighting
    We did it. It's actually possible. Faked lighting on an imaginary Z-axis. It's definitely messy (we need to convert to world space so bad, screen space has issues with the camera movement) but it will do for now. Finally, we can get back to more important topics.

---
+++ src/ZLighting/Scripts/z_light.gd
@@ -0,0 +1,137 @@
+@tool
+extends Area2D
+class_name ZLight
+
+const UNIT_SCALE : float = 100.0;
+const FALLOFF_SCALE : float = 10.0;
+
+@onready var collider : CollisionShape2D = $CollisionShape2D;
+@onready var falloff_prev : CollisionShape2D = $FalloffPrev;
+
+@export var shape : Shape2D :
+	set(value):
+		if value is CircleShape2D || value is RectangleShape2D || value == null :
+			shape = value;
+			
+			if collider == null :
+				return;
+			
+			collider.shape = shape;
+			_setup_shape_size();
+			_setup_falloff_prev();
+		else :
+			print("ERROR: Unsupported shape type!")
+
+
+@export var size : Vector2 = Vector2(1.0, 1.0) :
+	set(value) :
+		if value.x < 0 : value.x = 0;
+		if value.y < 0 : value.y = 0;
+		
+		size = value;
+		
+		_setup_shape_size();
+		
+		_setup_falloff_prev();
+
+
+@export var color : Color = Color.WHITE :
+	set(value) :
+		color = value;
+		# ZLIGHT_TBD: Runtime color changes
+
+
+@export var energy : float = 1.0 :
+	set(value) :
+		energy = value;
+		# ZLIGHT_TBD: Runtime energy changes 
+
+
+@export var falloff : float = 0.0 :
+	set(value) :
+		if value < 0 : value = 0;
+		var scale = 0;
+		
+		if shape is CircleShape2D :
+			scale = (shape as CircleShape2D).radius;
+		if shape is RectangleShape2D :
+			scale = max((shape as RectangleShape2D).size.x, (shape as RectangleShape2D).size.y);
+		
+		if (value * FALLOFF_SCALE) >= scale :
+			_setup_falloff_prev();
+			return;
+		
+		falloff = value;
+		_setup_falloff_prev();
+		# ZLIGHT_TBD: Runtime falloff changes 
+
+
+var _falloff_prev : Shape2D;
+
+
+func _ready() -> void:
+	if !Engine.is_editor_hint() :
+		falloff_prev.disabled = true;
+		falloff_prev.visible = false;
+	
+	collider.shape = shape;
+	
+	_setup_shape_size();
+	_setup_falloff_prev();
+
+
+func _process(delta: float) -> void:
+	pass;
+
+
+func get_size() -> Vector2:
+	return size * UNIT_SCALE;
+
+
+func get_falloff() -> Vector2:
+	if _falloff_prev is CircleShape2D :
+		var radius = (shape as CircleShape2D).radius;
+		radius -= (falloff * FALLOFF_SCALE);
+		return Vector2(radius, radius)
+	
+	if _falloff_prev is RectangleShape2D :
+		var size = (shape as RectangleShape2D).size;
+		var percent = (max(size.x, size.y) - (falloff * FALLOFF_SCALE)) / max(size.x, size.y);
+		return (size * percent);
+	
+	return Vector2.ZERO;
+
+
+func _setup_shape_size() :
+	if shape != null :
+		if shape is CircleShape2D :
+			(shape as CircleShape2D).radius = max(size.x, size.y) * UNIT_SCALE;
+			_falloff_prev = CircleShape2D.new();
+		
+		if shape is RectangleShape2D :
+			(shape as RectangleShape2D).size = size * UNIT_SCALE;
+			_falloff_prev = RectangleShape2D.new();
+
+
+
+func _setup_falloff_prev() :
+	if !Engine.is_editor_hint() :
+		return;
+	
+	if shape == null : 
+		if _falloff_prev != null : 
+			_falloff_prev = null;
+			return;
+	
+	if _falloff_prev is CircleShape2D :
+		var radius = (shape as CircleShape2D).radius;
+		radius -= (falloff * FALLOFF_SCALE);
+		(_falloff_prev as CircleShape2D).radius = radius;
+	
+	if _falloff_prev is RectangleShape2D :
+		var size = (shape as RectangleShape2D).size;
+		var percent = (max(size.x, size.y) - (falloff * FALLOFF_SCALE)) / max(size.x, size.y);
+		(_falloff_prev as RectangleShape2D).size = (size * percent)
+	
+	if falloff_prev != null :
+		falloff_prev.shape = _falloff_prev;
---
+++ src/ZLighting/Scripts/z_light_receiver.gd
@@ -0,0 +1,135 @@
+@tool
+extends Area2D
+class_name ZLightReceiver
+
+const MAX_LIGHTS_PER_OBJECT : int = 8;
+
+@onready var collider : CollisionShape2D = $CollisionShape2D;
+
+@export var shape : Shape2D :
+	set(value):
+		if value is RectangleShape2D || value == null :
+			shape = value;
+			
+			if collider == null :
+				return;
+			
+			collider.shape = shape;
+			_setup_shape_size();
+		else :
+			print("ERROR: Unsupported shape type!")
+
+
+@export var size : Vector2 = Vector2(50.0, 50.0) :
+	set(value) :
+		if value.x < 0 : value.x = 0;
+		if value.y < 0 : value.y = 0;
+		
+		size = value;
+		
+		_setup_shape_size();
+
+
+@export var static_receiver : bool = true;
+@export var falloff_y : bool = false;
+
+var _mat : ShaderMaterial;
+var _lights : Array[ZLight];
+var _sprite : Sprite2D;
+
+var _current_pos : Vector2;
+
+
+func _setup_shape_size() :
+	if shape != null :
+		if shape is RectangleShape2D :
+			(shape as RectangleShape2D).size = size;
+
+
+func _ready() -> void:
+	process_mode == ProcessMode.PROCESS_MODE_INHERIT;
+	
+	collider.shape = shape;
+	
+	if (get_parent() is Sprite2D) :
+		_sprite = (get_parent() as Sprite2D);
+	
+	if !Engine.is_editor_hint() :
+		_mat = (get_parent() as CanvasItem).material.duplicate() as ShaderMaterial;
+		(get_parent() as CanvasItem).material = _mat;
+		_current_pos = global_position;
+		_set_material_params();
+
+
+func _physics_process(delta: float) -> void:
+	if Engine.is_editor_hint() : return;
+	
+	if !static_receiver :
+		var dist = _current_pos.distance_squared_to(global_position);
+		
+		if dist > 0 :
+			_set_material_params();
+			_current_pos = global_position
+
+
+func _set_material_params() :
+	if _mat == null : return;
+	
+	# Define origin point for light in screenspace
+	var screen_pos = get_global_transform_with_canvas().origin;
+	
+	_mat.set_shader_parameter("origin", screen_pos);
+	_mat.set_shader_parameter("falloff_y", falloff_y);
+	#print("SCREEN POS: " + str(screen_pos))
+	
+	# Calculate light parameters
+	var light_origins : Array[Vector2];
+	var light_is_circle : Array[bool];
+	var light_size : Array[Vector2];
+	var light_color : Array[Color];
+	var light_energy : Array[float];
+	var light_falloff : Array[Vector2];
+	
+	for i in MAX_LIGHTS_PER_OBJECT :
+		if i < _lights.size() :
+			var light = _lights[i];
+			var light_screen_pos = light.get_global_transform_with_canvas().origin;
+			#print(light_screen_pos)
+			#print(light.get_size())
+			#print(light.get_falloff())
+			
+			light_origins.append(light_screen_pos);
+			light_is_circle.append(light.shape is CircleShape2D);
+			light_size.append(light.get_size());
+			light_color.append(light.color);
+			light_energy.append(light.energy);
+			light_falloff.append(light.get_falloff());
+		else :
+			light_origins.append(Vector2.ZERO);
+			light_is_circle.append(false);
+			light_size.append(Vector2.ZERO);
+			light_color.append(Color.TRANSPARENT);
+			light_energy.append(0.0);
+			light_falloff.append(Vector2.ZERO);
+	
+	_mat.set_shader_parameter("light_origins", light_origins);
+	_mat.set_shader_parameter("light_is_circle", light_is_circle);
+	_mat.set_shader_parameter("light_size", light_size);
+	_mat.set_shader_parameter("light_color", light_color);
+	_mat.set_shader_parameter("light_energy", light_energy);
+	_mat.set_shader_parameter("light_falloff", light_falloff);
+	
+	# Set active light count
+	_mat.set_shader_parameter("active_zlights", min(_lights.size(), MAX_LIGHTS_PER_OBJECT));
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area is ZLight && _lights.size() < MAX_LIGHTS_PER_OBJECT:
+		_lights.append(area as ZLight);
+		_set_material_params();
+
+
+func _on_area_exited(area: Area2D) -> void:
+	if area is ZLight && _lights.has(area as ZLight):
+		_lights.erase(area as ZLight);
+		_set_material_params();
