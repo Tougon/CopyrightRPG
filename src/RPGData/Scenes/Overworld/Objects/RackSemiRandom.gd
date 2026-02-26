65049c3f95e0ad5bb08d4f7c516b7d49e1f564cf
Evan McGrath <34551781+Tougon@users.noreply.github.com>
2026-02-26 14:32:47 -0800

    2-26-26 - Prop semi-randomization
    Added support to semi-randomize displays/shelves to save on having to manually alter every single one.

---
+++ src/RPGData/Scenes/Overworld/Objects/RackSemiRandom.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+@export var seed : int;
+@export var use_index_as_seed : bool = true;
+
+@export var items : Array[Sprite2D];
+@export var textures : Array[Texture2D];
+@export var colors : Array[Color];
+@export_range(1, 10) var visibility_chance : int = 2;
+
+func _ready() :
+	if items.size() == 0 || colors.size() == 0 :
+		return;
+	
+	var current = seed;
+	if use_index_as_seed : current = get_index();
+	
+	for item in items :
+		
+		if item != null :
+			var random = rand_from_seed(current)[0];
+			
+			item.visible = random % visibility_chance == 0;
+			item.texture = textures[random % textures.size()];
+			item.modulate = colors[random % colors.size()];
+			
+			current = random;
