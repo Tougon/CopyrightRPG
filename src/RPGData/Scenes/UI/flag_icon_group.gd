55426f00d57f77aadb1ce66e8a3db1970286588e
Evan McGrath <34551781+Tougon@users.noreply.github.com>
2026-09-17 22:13:42 -0700

    9-17-26 - Flag UI Fixes
    Refactored flag UI object so individual icons can be scaled.
    Fixed issue where flag UI would not be in the correct order.

--- src/RPGData/Scenes/UI/flag_icon_group.gd
+++ src/RPGData/Scenes/UI/flag_icon_group.gd
@@ -3,8 +3,10 @@ class_name FlagIconGroup
 
 @export var flags : Array[TFlag];
 @onready var icon_root : Control = $"Icon Container";
+@export var resize_icons : bool = true;
 
 var _icons : Array[TextureRect];
+var _icon_default_size : Vector2;
 
 func _ready():
 	for icon in icon_root.get_children() :
@@ -12,14 +14,27 @@ func _ready():
 		
 		if i != null :
 			_icons.append(i);
+		
+		_icon_default_size = i.custom_minimum_size;
 	
 	set_sealing(false);
 
 
 func display_flags(flags_to_display : Array[TFlag]):
-	
 	for i in flags.size():
 		_icons[i].visible = flags_to_display.has(flags[i]);
+		
+		if _icons[i].visible :
+			var index = flags_to_display.find(flags[i]);
+			_icons[i].get_parent().move_child(_icons[i], index);
+			
+			if index == 0 && resize_icons:
+				_icons[i].get_child(0).size = _icon_default_size * 1.25;
+				_icons[i].get_child(0).position = (_icons[i].get_child(0).size - _icon_default_size) / -2.0;
+				_icons[i].get_child(0).position.x -= 2.0;
+			else :
+				_icons[i].get_child(0).size = _icon_default_size;
+				_icons[i].get_child(0).position = Vector2.ZERO;
 
 
 func clear_flags():
@@ -29,4 +44,4 @@ func clear_flags():
 
 func set_sealing(sealing : bool):
 	for icon in _icons:
-		icon.get_child(0).visible = sealing;
+		icon.get_child(0).get_child(0).visible = sealing;
